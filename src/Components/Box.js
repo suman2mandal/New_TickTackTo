9d2a013d1b9a231a889aebc025620a679f745df0
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2023-07-02 18:34:30 +0530

    try different way

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Box({index,setNumber}) {
+
   return (
-    <button className="btn btn-primary col-4" onClick={setNumber(index)} >
-        <div key={{index}} style={{marginTop:"50%",marginBottom:"50%",height:"5px",textAlign:"center",verticalAlign:"center"}}>
+    <button className="btn btn-primary col-4" onClick={()=>setNumber(index)} >
+        {/* <div key={{index}} style={{marginTop:"50%",marginBottom:"50%",height:"5px",textAlign:"center",verticalAlign:"center"}}>
         {index}
+        </div> */}
+        <div>
+          {index}
         </div>
     </button>
   )
