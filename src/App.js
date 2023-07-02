5f7b7da7257ac36b43cf1f97f15a9a7339f66664
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2023-07-02 19:05:32 +0530

    Make the boxes pretty by manual css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Box from './Components/Box';
 import './App.css';
 import React from 'react';
 import { useState,useEffect } from 'react';
+import './assets/Extra.css';
 
 function App() {
   const setNumber = (index) => {
@@ -9,10 +10,20 @@ function App() {
   } 
   return (
   <React.Fragment>
-      <div className="container d-flex justify-content-center">
+      <div className="Wrapper">
         <Box setNumber={setNumber} index={1}/> 
         <Box setNumber={setNumber} index={2}/> 
         <Box setNumber={setNumber} index={3}/> 
+      </div>
+      <div className="Wrapper">
+        <Box setNumber={setNumber} index={4}/> 
+        <Box setNumber={setNumber} index={5}/> 
+        <Box setNumber={setNumber} index={6}/> 
+      </div>
+      <div className="Wrapper">
+        <Box setNumber={setNumber} index={7}/> 
+        <Box setNumber={setNumber} index={8}/> 
+        <Box setNumber={setNumber} index={9}/> 
       </div>
   </React.Fragment>
 
--- src/Components/Box.js
+++ src/Components/Box.js
@@ -3,14 +3,14 @@ import React from 'react'
 function Box({index,setNumber}) {
 
   return (
-    <button className="btn btn-primary col-4" onClick={()=>setNumber(index)} >
-        {/* <div key={{index}} style={{marginTop:"50%",marginBottom:"50%",height:"5px",textAlign:"center",verticalAlign:"center"}}>
-        {index}
-        </div> */}
-        <div>
+    // <button className="btn btn-primary col-4" onClick={()=>setNumber(index)} >
+        // {/* <div key={{index}} style={{marginTop:"50%",marginBottom:"50%",height:"5px",textAlign:"center",verticalAlign:"center"}}>
+        // {index}
+        // </div> */}
+        <div className="Box" onClick={()=>setNumber(index)}>
           {index}
         </div>
-    </button>
+    // </button>
   )
 }
 
